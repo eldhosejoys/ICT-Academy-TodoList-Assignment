89cbb16bb7edd3e1038d5fe38b73c3cba27b35eb
eldhose <eldho1998@gmail.com>
2021-11-30 18:30:26 +0530

    completed without Promise()

--- js/validatetodolist.js
+++ js/validatetodolist.js
@@ -1,9 +1,24 @@
 var datainfo = document.getElementById("datainfo");
+var count = 0;
 
 function selection(i) {
     var card = document.getElementById(i);
-    card.style.backgroundColor = "#ADFF2F";
-    card.setAttribute("selected", "1");
+    if (card.getAttribute("selected") == "1") {
+        card.style.backgroundColor = "#FFF";
+        card.setAttribute("selected", "0");
+        count = count - 1;
+    } else if (card.getAttribute("selected") == "0") {
+        card.style.backgroundColor = "#ADFF2F";
+        card.setAttribute("selected", "1");
+        count += 1;
+        if (count % 5 == 0) {
+            setTimeout(function() {
+                alert("Congrats. " + count + " Tasks have been Successfully Completed");
+            }, 10);
+
+        }
+    }
+
 }
 
 function requestjson() {
@@ -12,16 +27,14 @@ function requestjson() {
         if (this.readyState == 4 && this.status == 200) {
             var content = JSON.parse(this.responseText);
             var out = "";
+
             for (var i = 0; i < content.length; i++) {
-                var color = "";
                 var userid = content[i].userId;
-                if (userid % 2 == 0) { color = "background-color: antiquewhite;"; }
                 var id = content[i].id;
                 var title = content[i].title;
                 out += `
-                <div class="col pb-2"><div onclick='selection(${i});' class="card" style="${color}" slected="0" id="${i}">
+                <div class="col pb-2"><div onclick='selection(${i});' class="card" selected="0" id="${i}">
 <div class="card-body">
-<h1 class="card-title text-center pb-1 h5">User: ${userid}</h1>
 <span class="card-text">
 ${title}
 </span>
